f5df5235d16d0b90256664811410890291818340
Harshit Gupta <kohthecodemaster@gmail.com>
2019-07-09 21:47:24 +0530

    Init Commit. [Updated Launcher Icon]
    /*
     *  Date Created : 9th July 2K19, 09:30 PM..!!
     *  Last Modified : 9th July 2K19, 09:30 PM..!!
     *
     *  Change Log:
     *  Init Commit. [Updated Launcher Icon]
     *
     *  Code Developed By,
     *  ~K.O.H..!! ^__^
     */
    
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

---
+++ app/src/main/java/dev/koh/kohnotes/MainActivity.java
@@ -0,0 +1,25 @@
+package dev.koh.kohnotes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+/*
+ *  Date Created : 9th July 2K19, 09:30 PM..!!
+ *  Last Modified : 9th July 2K19, 09:30 PM..!!
+ *
+ *  Change Log:
+ *  Init Commit [Updated Launcher Icon]
+ *
+ *  Code Developed By,
+ *  ~K.O.H..!! ^__^
+ */
